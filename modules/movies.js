0985ebe5f85b92257d11bedc7fc545f1cb3bd3d2
Phil Chaplin <pchapl1@gmail.com>
2023-03-09 13:17:31 -0800

    updated getWeather method

---
+++ modules/movies.js
@@ -0,0 +1,35 @@
+'use strict';
+const axios = require('axios')
+
+async function getMovies(req, res, next){
+    console.log('in get movies')
+    try {
+        let cityToFind = req.query.city
+
+        let params = {
+            api_key: process.env.TMDB_API_KEY,
+            query: cityToFind
+        }
+
+        let url = `https://api.themoviedb.org/3/search/movie`
+
+        let resultMovies = await axios.get(url, { params })
+
+        res.send(resultMovies.data.results)
+    } catch (error) {
+        Promise.resolve().then(()=>{
+            throw new Error(error.message);
+        }).catch(next)
+    }
+
+}
+
+class Movie {
+    constructor(movieObject) {
+        this.title = movieObject.title,
+        this.overview = movieObject.overview, 
+        this.image = movieObject.image_url
+    }
+}
+
+module.exports = getMovies;
--- server.js
+++ server.js
@@ -4,10 +4,11 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const axios = require('axios')
+const axios = require('axios');
 
-//syntax for getting data 
-let weatherData = require("./weather.json")
+const getWeather = require('./modules/weather');
+const getMovies = require('./modules/movies');
+
 
 //USE
 const app = express();
@@ -16,33 +17,12 @@ app.use(cors());
 
 // ROUTES
 app.get(`/`, ( req, res )=>{
-    res.send('hello from our server')
-})
+    res.send('hello from our server');
+});
 
-app.get('/weather', async (req, res, next) => {
-    let cityToFind = req.query.city
+app.get('/weather', getWeather );
 
-    let url = `https://api.weatherbit.io/v2.0/forecast/daily?&city=${cityToFind}&key=${process.env.WEATHER_API_KEY}&days=3`
-
-    console.log(url)
-
-    try {
-        let resultCity = await axios.get(url)
-        console.log(resultCity.data)
-        let selectedCity = resultCity.data.data.map(item => new ForeCast(item))
-        res.send(selectedCity)
-    } catch (error) {
-        console.log(error)
-    }
-
-})
-
-class ForeCast {
-    constructor(WeatherObject) {
-        this.date = WeatherObject.valid_date,
-        this.description = `Low of ${WeatherObject.low_temp}, high of ${WeatherObject.max_temp} with ${WeatherObject.weather.description.toLowerCase()}`
-    }
-}
+app.get('/movies', getMovies);
 
 
 

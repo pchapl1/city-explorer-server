7b7c60eab29c91b42922f3a657ec2e7d5b0bb0a3
Phil Chaplin <pchapl1@gmail.com>
2023-03-09 15:52:45 -0800

    worked on lab 9

--- modules/movies.js
+++ modules/movies.js
@@ -2,33 +2,32 @@
 const axios = require('axios')
 
 async function getMovies(req, res, next){
-    // console.log('in get movies')
-    try {
-        let cityToFind = req.query.city
+    let cityToFind = req.query.city
 
-        let params = {
-            api_key: process.env.TMDB_API_KEY,
-            query: cityToFind
-        }
-
-        let url = `https://api.themoviedb.org/3/search/movie`
-
-        let resultMovies = await axios.get(url, { params })
-
-        res.send(resultMovies.data.results)
-    } catch (error) {
-        Promise.resolve().then(()=>{
-            throw new Error(error.message);
-        }).catch(next)
+    let params = {
+        api_key: process.env.TMDB_API_KEY,
+        query: cityToFind
     }
 
+    let url = `https://api.themoviedb.org/3/search/movie`
+    let test = await axios.get(url, {params})
+    console.log(test.data.results)
+    axios.get(url, { params })
+        .then(resultsFromApi => resultsFromApi.data.results.map(item => new Movie(item)))
+        .then(dataToSend => res.status(200).send(dataToSend))
+        .catch(err => console.error(err))
 }
 
 class Movie {
     constructor(movieObject) {
+        this.id = movieObject.id,
         this.title = movieObject.title,
         this.overview = movieObject.overview, 
-        this.image = movieObject.image_url
+        this.vote_average = movieObject.vote_average,
+        this.vote_count = movieObject.vote_count,
+        this.popularity = movieObject.popularity,
+        this.release_date = movieObject.release_date,
+        this.poster_path = movieObject.poster_path
     }
 }
 
--- modules/weather.js
+++ modules/weather.js
@@ -2,25 +2,25 @@
 const axios = require('axios')
 
 
-async function getWeather (req, res, next){
-        console.log('in get weather')
-        let cityToFind = req.query.city
+function getWeather (req, res, next){
+    
+    let cityToFind = req.query.city
 
-        let params = {
-            city: cityToFind, 
-            key: process.env.WEATHER_API_KEY,
-            days: 3
-        }
+    let params = {
+        city: cityToFind, 
+        key: process.env.WEATHER_API_KEY,
+        days: 3
+    }
 
-        let url = `https://api.weatherbit.io/v2.0/forecast/daily`;
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily`;
 
-        // let data = await axios.get(url, {params})
-        // console.log(data)
+    // let data = await axios.get(url, {params})
+    // console.log(data)
 
-        axios.get(url, {params})
-            .then(resultsFromApi => resultsFromApi.data.data.map(item => new ForeCast(item)))
-            .then(dataToSend => res.status(200).send(dataToSend))
-            .catch(err => console.error(err))
+    axios.get(url, {params})
+        .then(resultsFromApi => resultsFromApi.data.data.map(item => new ForeCast(item)))
+        .then(dataToSend => res.status(200).send(dataToSend))
+        .catch(err => console.error(err))
 
 }
 
